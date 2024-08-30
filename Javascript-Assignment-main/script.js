7f34e13b9837b6c6cb4768b85243f48a0a4a6d9e
Zainul Abideen <121451360+Zain1216@users.noreply.github.com>
2024-08-30 19:57:30 +0500

    Add files via upload

---
+++ Javascript-Assignment-main/script.js
@@ -0,0 +1,27 @@
+let percantAge = prompt('Enter your Percantage:');
+
+if(percantAge < 30) {
+     console.log('F')}
+
+ else if(percantAge <= 40) {
+console.log('D')}
+ else if(percantAge <= 50) {
+            console.log('C')}
+else if (percantAge <= 60) {
+ console.log('B')
+                  }
+                
+ else if (percantAge <= 70) {
+ console.log('A');}
+
+else if (percantAge <= 80) {
+ console.log('A+')
+                    }
+               
+else if (percantAge <= 100) 
+    {console.log('A+') }
+
+
+else{
+    console.log('Kindly enter valid Percantage')
+}
